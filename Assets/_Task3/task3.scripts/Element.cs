566ab9ed6e332dd1b82c5a529cf5a417d7a6555b
avdunitydev <avdunitydev@users.noreply.github.com>
2017-04-20 15:25:16 +0300

    cw

---
+++ Assets/_Task3/task3.scripts/Element.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Element : MonoBehaviour
+{
+	public int posX;
+	public int posY;
+	public Sprite[] itemSprites;
+	SpriteRenderer sprr;
+
+	// Use this for initialization
+	void Start ()
+	{
+		sprr = GetComponent<SpriteRenderer> ();
+		sprr.sprite = itemSprites [Random.Range (0, itemSprites.Length)];
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
---
+++ Assets/_Task3/task3.scripts/game3.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game3 : MonoBehaviour
+{
+	public Transform element;
+	public Element[,] elements;
+	int sizeX;
+	int sizeY;
+
+	// Use this for initialization
+	void Start ()
+	{
+		sizeX = 20;
+		sizeY = 50;
+		elements = new Element[sizeX, sizeY];
+
+		for (int i = 1; i < sizeX - 1; ++i) {
+			for (int j = 1; j < sizeY - 1; ++j) {
+				Transform newElement = Instantiate (element, new Vector3 (j, i, 0), 
+					                       Quaternion.identity, transform);
+				Element getItem = newElement.GetComponent<Element> ();
+				getItem.posX = j;
+				getItem.posY = i;
+				elements [j, i] = getItem;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
