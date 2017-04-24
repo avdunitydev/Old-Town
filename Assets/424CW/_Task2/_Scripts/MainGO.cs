d6527dcd5394d9afb94e10a672fc44c2c7a1adee
avdunitydev <avdunitydev@users.noreply.github.com>
2017-04-24 13:51:06 +0300

    move Ball

---
+++ Assets/424CW/_Task2/_Scripts/MainGO.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainGO : MonoBehaviour
+{
+	public Transform cube;
+	public Transform ball;
+	public float speed;
+
+
+	int rnd ()
+	{
+		return Random.Range (-14, 14);
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		Instantiate (ball, new Vector3 (rnd (), rnd (), 0), Quaternion.identity, transform);
+		Instantiate (cube, new Vector3 (rnd (), rnd (), 0), Quaternion.identity, transform);
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+
+	}
+}
---
+++ Assets/424CW/_Task2/_Scripts/SphereModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereModel : MonoBehaviour
+{
+	public Material[] materials;
+	public float speed;
+	CubeModel target;
+	MainGO fObj;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		fObj = GetComponentInParent<MainGO> ();
+		//MeshRenderer<material>()= materials [Random.Range (0, materials.Length)];
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		float step = speed * Time.deltaTime;
+		transform.position = Vector3.MoveTowards (transform.position, fObj.GetComponentInChildren<CubeModel> ().transform.position, step);
+	}
+}
