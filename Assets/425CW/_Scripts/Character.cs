75aee37c1e3ebf92d0916f2656caae77fcb9b440
avdunitydev <avdunitydev@users.noreply.github.com>
2017-04-25 15:21:14 +0300

    miniGame

---
+++ Assets/425CW/_Scripts/Character.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+	public float speed;
+	float step;
+	public Rigidbody2D fire;
+	Rigidbody2D mZombie;
+	Vector2 fireDirection;
+	SpriteRenderer sprr;
+
+	// Use this for initialization
+	void Start ()
+	{
+		sprr = GetComponent<SpriteRenderer> ();
+		mZombie = GetComponent<Rigidbody2D> ();
+		fireDirection = new Vector2 (-1, 0);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		step = speed * Time.deltaTime;
+	}
+
+	void FixedUpdate ()
+	{
+		if (Input.GetKey (KeyCode.W)) {
+			mZombie.velocity = new Vector2 (mZombie.velocity.x, step);
+		} else if (Input.GetKey (KeyCode.S)) {
+			mZombie.velocity = new Vector2 (mZombie.velocity.x, -step);
+		}
+
+		if (Input.GetKey (KeyCode.A)) {
+			sprr.flipX = false;
+			mZombie.velocity = new Vector2 (-step, mZombie.velocity.y);
+		} else if (Input.GetKey (KeyCode.D)) {
+			sprr.flipX = true;
+			mZombie.velocity = new Vector2 (step, mZombie.velocity.y);
+		}
+	}
+}
---
+++ Assets/425CW/_Scripts/TestColl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Тест подій
+public class TestColl : MonoBehaviour
+{
+
+	void OnCollisionEnter2D (Collision2D coll)
+	{
+		print ("OnCollisionEnter2D " + name + " * " + coll.transform.name);
+	}
+
+	void OnCollisionStay2D (Collision2D coll)
+	{
+		print ("OnCollisionStay2D " + name + " * " + coll.transform.name);
+	}
+
+	void OnCollisionExit2D (Collision2D coll)
+	{
+		print ("OnCollisionExit2D " + name + " * " + coll.transform.name);
+	}
+
+	void OnTriggerEnter2D (Collider2D coll)
+	{
+		print ("OnTriggerEnter2D " + name + " * " + coll.name);
+	}
+
+	void OnTriggerStay2D (Collider2D coll)
+	{
+		print ("OnTriggeStay2D " + name + " * " + coll.name);
+	}
+
+	void OnTriggerExit2D (Collider2D coll)
+	{
+		print ("OnTriggerExit2D " + name + " * " + coll.name);
+	}
+}
